02fcadd0e0febc6c9fcbfd97ebc861725548674f
Mohamed EL MASNAOUI <mohamed.elmasnaoui@intalio.com>
2024-07-16 17:10:28 +0100

    Build a Dynamic To-Do List Application

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Select DOM elements
     const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
@@ -16,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const removeButton = document.createElement('button');
         removeButton.textContent = "Remove";
-        removeButton.className = 'remove-btn';
+        removeButton.classList.add('remove-btn');
 
         removeButton.onclick = () => {
             taskList.removeChild(taskItem);
